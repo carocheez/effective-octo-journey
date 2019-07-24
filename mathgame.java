f2eb92a21290dea24b54b15ea1d67e5e8eceb530
Carolyn Chen <cgc731@gmail.com>
2019-07-23 18:29:37 -0700

    Update mathgame.java

--- mathgame.java
+++ mathgame.java
@@ -1,3 +1,22 @@
 // math game 
 
 
+import java.util.*;
+import java.util.Random; 
+
+
+class Main {
+
+  public static int a = 0;
+  public static int b = 0;
+
+  public static void main(String[] args) {
+    System.out.println(generateProblem());
+  }
+
+  public static String generateProblem(){
+    a = (int)(Math.random() * 9) + 1;
+    b = (int)(Math.random() * 9) + 1;
+    return Integer.toString(a) + " + " + Integer.toString(b);
+  }
+}
